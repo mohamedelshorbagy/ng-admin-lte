e7d908115153423f5c285eb47f28fc04d0ac6a6e
mohamedelshorbagy <mohamedelshorbagy96@gmail.com>
2020-04-08 03:45:50 +0200

    :tada: inital commit & with base setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'admin-lte';
+  title = 'admin-lte-angular';
+  sideBarOpened: boolean = false;
+
+
+
+  toggleSideBar() {
+    this.sideBarOpened = !this.sideBarOpened;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
